97acadb01b6c3e5efa9a7c817af915dc32b2527f
Wu <s-wube@bsd405.org>
2024-02-16 13:10:06 -0800

    Removed slots, working on updating array

--- Assets/Scripts/Spelling Bee/TileSlot.cs
+++ Assets/Scripts/Spelling Bee/TileSlot.cs
@@ -8,8 +8,14 @@ public class TileSlot : MonoBehaviour
     public void OnDrop(BaseEventData data) {
         PointerEventData eventData = (PointerEventData) data;
 
-        GameObject dropped = eventData.pointerDrag;
-        TileDrag tileDrag = dropped.GetComponent<TileDrag>();
-        tileDrag.parentAfterDrag = transform;
+        /* GameObject dropped = eventData.pointerDrag;
+        LetterSquare square = dropped.GetComponent<LetterSquare>();
+        square.parentAfterDrag = transform; */
+
+        if (eventData.pointerDrag != null) {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            Debug.Log("dropped");
+        }
+        // Debug.Log("dropped");
     }
 }
