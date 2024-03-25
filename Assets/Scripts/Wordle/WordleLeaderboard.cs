02130096b9cf325c05cc8ceb46c7db9183c471a5
Wu <s-wube@bsd405.org>
2024-03-25 13:10:00 -0700

    Basic leaderboard UI

---
+++ Assets/Scripts/Wordle/WordleLeaderboard.cs
@@ -0,0 +1,93 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.Services.Core;
+using Unity.Services.Leaderboards;
+using Unity.Services.Authentication;
+
+public class WordleLeaderboard : MonoBehaviour
+{
+    public string leaderboardId = "shs_wordle";
+    public LeaderboardScoreView scoreViewPrefab;
+
+    public TextMeshProUGUI messageText;
+    public TMP_InputField scoreInputField;
+    public Button submitScoreButton;
+    public Button loadScoresButton;
+    public Transform scoresContainer;
+
+    private async void Awake() {
+        await UnityServices.InitializeAsync();
+    }
+
+    private async void Start() {
+        AuthenticationService.Instance.SignedIn += OnSignedIn;
+        AuthenticationService.Instance.SignInFailed += OnSignInFailed;
+
+        messageText.text = "Signing in...";
+
+        // Players sign in anonymously
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+        submitScoreButton.onClick.AddListener(SubmitScoreAsync);
+        loadScoresButton.onClick.AddListener(LoadScoresAsync);
+    }
+
+    // When player signs in successfully
+    private void OnSignedIn() {
+        messageText.text = $"Signed in as: {AuthenticationService.Instance.PlayerId}";
+    }
+
+    // When player sign in fails
+    private void OnSignInFailed(RequestFailedException exception) {
+        messageText.text = $"Sign in failed with exception: {exception}";
+    }
+
+    private async void SubmitScoreAsync() {
+        if (string.IsNullOrEmpty(scoreInputField.text)) {
+            return;
+        }
+
+        var score = Convert.ToDouble(scoreInputField.text);
+        scoreInputField.text = string.Empty;
+
+        try {
+            await LeaderboardsService.Instance.AddPlayerScoreAsync(leaderboardId, score);
+            messageText.text = "Score submitted!";
+        }
+
+        catch (Exception e) {
+            messageText.text = $"Failed to submit score: {e}";
+            throw;
+        }
+    }
+
+    private async void LoadScoresAsync() {
+        try {
+            var scoresResponse = await LeaderboardsService.Instance.GetScoresAsync(leaderboardId);
+            var childCount = scoresContainer.childCount;
+
+            for (int i = 0; i < childCount; i++) {
+                Destroy(scoresContainer.GetChild(i).gameObject);
+            }
+
+            foreach (var leaderboardEntry in scoresResponse.Results) {
+                var scoreView = Instantiate(scoreViewPrefab, scoresContainer);
+                scoreView.Initialize(leaderboardEntry.Rank.ToString(), leaderboardEntry.PlayerName,
+                    leaderboardEntry.Score.ToString());
+            }
+
+            messageText.text = "Scores fetched!";
+        }
+        catch (Exception e) {
+            messageText.text = $"Failed to fetch scores: {e}";
+            throw;
+        }
+    }
+
+    private void OnDestroy() {
+        AuthenticationService.Instance.SignedIn -= OnSignedIn;
+        AuthenticationService.Instance.SignInFailed -= OnSignInFailed;
+    }
+}
