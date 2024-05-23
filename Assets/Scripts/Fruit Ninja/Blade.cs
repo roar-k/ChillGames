43bc643cb9a5738f63ae65e45c65139f7898f111
Wu <s-wube@bsd405.org>
2024-05-23 13:25:14 -0700

    Working on blade

--- Assets/Scripts/Fruit Ninja/Blade.cs
+++ Assets/Scripts/Fruit Ninja/Blade.cs
@@ -4,15 +4,67 @@ using UnityEngine;
 
 public class Blade : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private Camera mainCamera;
+    private Collider bladeCollider;
+
+    private bool slicing;
+
+    public Vector3 direction { get; private set; }
+    public float minSliceVelocity = 0.01f;
+
+    private void Awake() {
+        mainCamera = Camera.main;
+        bladeCollider = GetComponent<Collider>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnEnable() {
+        StopSlicing();
     }
+
+    private void OnDisable() {
+        StopSlicing();
+    }
+
+    private void Update() {
+        // Slices fruits when mouse button is held down
+        if (Input.GetMouseButtonDown(0)) {
+            StartSlicing();
+        }
+
+        // Stops slicing fruit when mouse button is not held down
+        else if (Input.GetMouseButtonDown(0)) {
+            StopSlicing();
+        }
+
+        else if (slicing) {
+            ContinueSlicing();
+        }
+    }
+
+    private void StartSlicing() {
+        transform.position = newPosition;
+
+        slicing = true;
+        bladeCollider.enabled = true;
+    }
+
+    private void StopSlicing() {
+        slicing = false;
+        bladeCollider.enabled = false;
+    }
+
+    // Blade follows the mouse cursor
+    private void ContinueSlicing() {
+        Vector3 newPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        newPosition.z = 0f;
+
+        direction = newPosition - transform.position;
+
+        float velocity = direction.magnitude / Time.deltaTime;
+        bladeCollider.enabled = velocity > minSliceVelocity;
+
+        transform.position = newPosition;
+    }
+
+
 }
